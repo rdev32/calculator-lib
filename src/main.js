4cadfec4b4512f65af3a99d0e23bcf76a69e05a1
donatto-minaya <ottanod22@gmail.com>
2021-12-23 02:03:02 -0500

    Modified

--- src/main.js
+++ src/main.js
@@ -7,5 +7,3 @@ import { fractions } from './arithmetic/fractions.js';
 export const BasicOperations = basicOperations;
 export const Area = area;
 export const Fractions = fractions; 
-
-console.log(Fractions.sum(1,2,-4,6));
