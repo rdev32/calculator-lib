d907df3692712d7bdb8d445364795317420dd7c3
donatto-minaya <ottanod22@gmail.com>
2021-12-31 09:44:58 -0500

    Percentage complete

--- src/arithmetic/percentage.js
+++ src/arithmetic/percentage.js
@@ -1,27 +1,23 @@
 export class percentage {
     /**
-    What percentage is?... (wpi)
-
     Example: What percentaje is 12 out of 30?
     @description Calculates the percentage of the total according to the value you give it
     @param {number} number 12
     @param {number} total 30
     **/
-    static wpi(number, total) {
+    static whatPercentageIs(number, total) {
         var result = (number * 100) / total;
         return String(result.toFixed(2)) + "%";
     }
 
-    /**
-    Percentage Of.. (pof)
-    
+    /**    
     Example: How much is 7% of 39?
     @description Calculates the percentage of a number or quantity.
     @param {number} percentage 7%
     @param {number} total 39
     **/
 
-    static pof(percentage, total) {
+    static percentageOf(percentage, total) {
         var result = (percentage * total) / 100;
         return String(result)
     }
@@ -36,6 +32,25 @@ export class percentage {
 
     static percentFromPercent(percentage1, value1, percentage2) {
         var result = (value1 * percentage2) / percentage1;
+
+        if(Number.isInteger(result)) {
+            return String(result);
+        }
+
+        else {
+            return String(result.toFixed(2));
+        }
+    }
+
+    /**
+    Example: If 10% is 20, the total is...
+    @description Calculate the total from a percentage
+    @param {number} percentage 10%
+    @param {number} value 20
+    **/
+
+    static totalFromPercent(percentage, value) {
+        var result = (value * 100) / percentage;
 
         if(Number.isInteger(result)) {
             return String(result);
