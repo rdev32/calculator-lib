f58e5c321a0e80315b6fd933f12527db190155cc
donatto-minaya <ottanod22@gmail.com>
2022-01-13 01:43:44 -0500

    F actorial and combinatorial numbers moved from basic operations to general operations.

--- src/general/operations.js
+++ src/general/operations.js
@@ -84,4 +84,65 @@ export class operations {
             }
         }
     }
+
+    /**
+    - Return the factorial of a number
+
+    @param {number} number 
+    **/
+
+    static factorial(number) {
+        return factorial(number, 1);
+    }
+
+    static doubleFactorial(number) {
+        return factorial(number, 2);
+    }
+
+    /**
+    - Example -> C¹¹₇
+    - Returns the number of possible combinations
+
+    @param {number} top - 11
+    @param {number} bottom - 7
+    **/
+    static combinatorial(top, bottom) {
+        if(typeof top === 'string' || typeof bottom === 'string') {
+            return "You must enter a number"
+        }
+
+        else {
+            return factorial(top) / (factorial(top - bottom) * factorial(bottom))
+        }
+    }
+}
+
+function factorial(number, decrease) {
+    var result;
+
+    if(typeof number === 'string') {
+        return "You must enter a number"
+    }
+
+    else if(number < 0) {
+        return "For now, it only receives positive numbers"
+    }
+
+    else {
+        if(number === 0 || number === 1) {
+            result = 1;
+        }
+
+        else if(number === 2 && decrease === 2) {
+            result = 2;
+        }
+
+        else {
+            for(var i = number - decrease; i >= 1; i-=decrease) {
+                result = (number *= i)
+            }
+        }
+
+        return result;
+    }
 }
